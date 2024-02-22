c667b606b8345d413f868a36762e122be0307472
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-02-22 16:32:46 -0300

    template init da app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client-app';
+export class AppComponent implements AfterViewInit {
+ 
+    title = 'client-app';
+
+    ngAfterViewInit(): void {
+        $(document).ready(() => {
+            const sidebarToggle = $('#sidebarToggle');
+            if (sidebarToggle.length) {
+                if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+                     $('body').addClass('sb-sidenav-toggled');
+                }
+                sidebarToggle.on('click', (event: { preventDefault: () => void; }) => {
+                    event.preventDefault();
+                    $('body').toggleClass('sb-sidenav-toggled');
+                    localStorage.setItem('sb|sidebar-toggle', $('body').hasClass('sb-sidenav-toggled').toString());
+                });
+            }
+        });
+    }
 }
