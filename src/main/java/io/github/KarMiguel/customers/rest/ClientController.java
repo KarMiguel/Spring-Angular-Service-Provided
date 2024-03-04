f295927dbcae66e880fb70659e3b7bf7dac02a11
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-03-03 21:45:33 -0300

    feat: change rota

--- src/main/java/io/github/KarMiguel/customers/rest/ClientController.java
+++ src/main/java/io/github/KarMiguel/customers/rest/ClientController.java
@@ -12,7 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/client")
+@RequestMapping("api/client/")
 public class ClientController {
 
     private final ClientRepository repository;
