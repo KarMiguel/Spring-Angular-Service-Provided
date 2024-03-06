204286655f4696d09fdb007bb510f3b5db8432c2
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-03-06 00:26:36 -0300

    feat: created model e repository do user da app

--- src/main/java/io/github/KarMiguel/customers/model/entity/Users.java
+++ src/main/java/io/github/KarMiguel/customers/model/entity/Users.java
@@ -3,16 +3,22 @@ package io.github.KarMiguel.customers.model.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Users {
+@AllArgsConstructor
+public class Users  implements UserDetails{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,12 +34,39 @@ public class Users {
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime created_at;
 
+    public Users(String username, String password, String name){
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
     @PrePersist
     public  void prePersist(){
         setCreated_at(LocalDateTime.now());
     }
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/io/github/KarMiguel/customers/model/repository/UserRepository.java
+++ src/main/java/io/github/KarMiguel/customers/model/repository/UserRepository.java
@@ -2,7 +2,12 @@ package io.github.KarMiguel.customers.model.repository;
 
 import io.github.KarMiguel.customers.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users,Integer> {
+    UserDetails findByUsername(String username);
 
+    boolean existsByUsername(String login);
 }
